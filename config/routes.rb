c6878b74bd87adce453a0d66bc8d45f954292f50
Chris Reeve <paradigmsareconstructed@gmail.com>
2015-05-24 08:57:29 -0700

    Added Annotator 2.0 functionality

--- config/routes.rb
+++ config/routes.rb
@@ -1,70 +1,28 @@
 Rails.application.routes.draw do
   get '/about' => 'about#index'
+
   devise_for :users
   resources :users, only: :show
+
   resources :pictures, only: :create
   resources :pictures, only: :destroy
-  resources :posts do
-  resources :comments
-  member do
-    put "like", to: "posts#upvote"
-    put "dislike", to: "posts#downvote"
-  end
-end 
 
   root to: 'posts#index'
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
+  resources :posts do
+    resources :comments
+    member do
+      put "like", to: "posts#upvote"
+      put "dislike", to: "posts#downvote"
+    end
+  end
 
-  # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  # Annotator.js routes
+  get 'annotator/' => 'annotation#root'
+  get 'annotator/annotations' => 'annotation#index'
+  post 'annotator/annotations' => 'annotation#create'
+  get 'annotator/annotations/:id' => 'annotation#read', as: 'annotation_read'
+  put 'annotator/annotations/:id' => 'annotation#update'
+  delete 'annotator/annotations/:id' => 'annotation#delete'
+  get 'annotator/search' => 'annotation#search'
 
-  # Example of regular route:
-  #   get 'products/:id' => 'catalog#view'
-
-  # Example of named route that can be invoked with purchase_url(id: product.id)
-  #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
-
-  # Example resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
-
-  # Example resource route with options:
-  #   resources :products do
-  #     member do
-  #       get 'short'
-  #       post 'toggle'
-  #     end
-  #
-  #     collection do
-  #       get 'sold'
-  #     end
-  #   end
-
-  # Example resource route with sub-resources:
-  #   resources :products do
-  #     resources :comments, :sales
-  #     resource :seller
-  #   end
-
-  # Example resource route with more complex sub-resources:
-  #   resources :products do
-  #     resources :comments
-  #     resources :sales do
-  #       get 'recent', on: :collection
-  #     end
-  #   end
-
-  # Example resource route with concerns:
-  #   concern :toggleable do
-  #     post 'toggle'
-  #   end
-  #   resources :posts, concerns: :toggleable
-  #   resources :photos, concerns: :toggleable
-
-  # Example resource route within a namespace:
-  #   namespace :admin do
-  #     # Directs /admin/products/* to Admin::ProductsController
-  #     # (app/controllers/admin/products_controller.rb)
-  #     resources :products
-  #   end
 end
---
+++ test/controllers/root_controller_test.rb
@@ -0,0 +1,34 @@
+require 'test_helper'
+
+class RootControllerTest < ActionController::TestCase
+  test "should get index" do
+    get :index
+    assert_response :success
+  end
+
+  test "should get create" do
+    get :create
+    assert_response :success
+  end
+
+  test "should get read" do
+    get :read
+    assert_response :success
+  end
+
+  test "should get update" do
+    get :update
+    assert_response :success
+  end
+
+  test "should get delete" do
+    get :delete
+    assert_response :success
+  end
+
+  test "should get search" do
+    get :search
+    assert_response :success
+  end
+
+end
